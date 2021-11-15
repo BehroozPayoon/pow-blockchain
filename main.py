6b9381513f29f9ac684c46b7b21b59f55caea7db
Behrooz Payoon <Behrooz.payoon@gmail.com>
2021-11-15 19:36:07 +0330

    Add fastapi to test blockchain proof of work method

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from blockchain import Blockchain
 
+# Building Blockchain
+blockchain = Blockchain()
+
+# Web App
 app = FastAPI()
 
-# Building Blockchain
+@app.get('/mineBlock')
+async def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    return {'message': 'You mined a block', 'data': block}
+
+@app.get('/getChain')
+async def get_chain():
+    return blockchain.chain
